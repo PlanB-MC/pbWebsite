e64362d58a63eb91d0c7cb7f39a64c2b3d3c2cdd
MattCSmith <mrmcchow@gmail.com>
2018-07-06 14:14:21 +0100

    init Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import General from './Layouts/general';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <General />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Layouts/general.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class general extends Component {
+  render() {
+    return (
+      <div>
+        <h1> General Page </h1>
+      </div>
+    );
+  }
+}
+
+export default general;

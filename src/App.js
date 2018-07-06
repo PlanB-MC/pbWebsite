14bd44f36ff072bf31b2bbb89c0ccb1f733cfcad
MattCSmith <mrmcchow@gmail.com>
2018-07-06 15:09:21 +0100

    Travis test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <General />
+      Header  
+      <General />
+        Footer
       </div>
     );
   }

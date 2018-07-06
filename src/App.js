88498a8a66bd5f27d9311b030d9611525af26b4d
MattCSmith <mrmcchow@gmail.com>
2018-07-06 15:49:04 +0100

    Implemented Basic routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,61 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"; 
+import './Css/App.css';
 import General from './Layouts/general';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      Header  
-      <General />
-        Footer
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Link to="/">Home</Link> - <Link to="/about">About</Link> - <Link to="/topics">Topics</Link>
+      
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/topics" component={Topics} />
+    </div>
+  </Router>
+);
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <General />
+  </div>
+);
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic} />
+    <Route
+      exact
+      path={match.url}
+      render={() => <h3>Please select a topic.</h3>}
+    />
+  </div>
+);
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+);
 
 export default App;

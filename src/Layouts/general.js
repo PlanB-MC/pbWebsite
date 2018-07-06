df8a9cd4f7ac996c5306866e4e46dae5616da579
MattCSmith <mrmcchow@gmail.com>
2018-07-06 14:49:50 +0100

    General Layout Structure

--- src/Layouts/general.js
+++ src/Layouts/general.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import './general.css'
 
 class general extends Component {
   render() {
     return (
-      <div>
-        <h1> General Page </h1>
-      </div>
+        <div className="parrallax" style={{ backgroundImage: "url(https://api.planb-mc.com/webImages/about1.png)"}}>
+
+        <div className="headerSection">
+            <div className="titleBar py-4">
+                <h1 className="text-center text-white">Title</h1>
+                <h2 className="text-center text-danger">SubTitle</h2>
+            </div>
+        </div>
+
+        <div className="transp"> 
+        
+            <div className="container py-4">
+                <div className="px-5 py-4 bg-dark introBox">
+                    <p className="text-white text-justify m-0">
+                        Description
+                    </p>
+                </div>
+            </div>
+
+            <div className="container py-4">
+                <div className="card-deck">
+
+                    <div className="card p-4">
+                        BOX 1
+                    </div>
+                        
+                    <div className="card p-4">
+                        BOX 2
+                    </div>
+                    
+                </div>
+            </div>
+            
+        </div>
+    </div>
     );
   }
 }

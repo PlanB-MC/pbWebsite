3f0434f8db58c316e795a045b561b5c1803a8c1f
MattCSmith <mrmcchow@gmail.com>
2018-07-12 03:19:02 +0100

    Added Server Descriptions

--- src/Components/LandingPage/serverCards.js
+++ src/Components/LandingPage/serverCards.js
@@ -20,7 +20,7 @@ const whichMenu = (keys) => {
                 <img className="card-img-top img-fluid rounded" src={svr.image} alt={svr.name}/>
                 <div className="card-body px-4 pt-3">
                     <h3 className="card-title mb-2 serverCardTitle">{svr.name}</h3>
-                    <p className="card-text mb-4 text-muted sCardSubtitle">{svr.desc}</p>
+                    <p className="card-text mb-4 text-muted text-justify sCardSubtitle">{svr.desc}</p>
                     <Link to={svr.infoLink} className="btn btn-outline-dark d-block">View </Link>
                 </div>
             </div>

f67fba87d8c3540e4b3594ccb077279250ff5e58
MattCSmith <mrmcchow@gmail.com>
2018-07-12 03:22:25 +0100

    Updated Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"; 
+import { BrowserRouter as Router, Route } from "react-router-dom"; 
 import './Css/App.css';
 import Header from './Components/Global/header';
 import ScrollToTop from './Components/Global/scrollTop';

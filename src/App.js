97dd2e84b0a4161638851cb13ef1b62bb02f4c19
MattCSmith <mrmcchow@gmail.com>
2018-09-22 22:25:54 +0100

    Route Adjustment

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route} from "react-router-dom"; 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"; 
 import './Css/App.css';
-
+import Header from './Components/Global/header';
+import ScrollToTop from './Components/Global/scrollTop';
 import LandingPage from './Pages/landingPage';
 import About from './Pages/aboutUs'
-
+import server from './Pages/serverPage'
+import Member from './Pages/members';
+import Quests from './Pages/questPage';
+import Tutorial from './Pages/tutorialPage';
+import Maps from './Pages/mapPage';
+import Footer from './Components/Global/footer';
 import application from './Pages/application'
 
 console.log(process.env.REACT_APP_TEST_TOKEN)
 const App = () => (
-<BrowserRouter>
-  <Switch>
-  <Route exact path="/" component={LandingPage}/>
-  <Route path="/about" component={About}/>
-  <Route path="/apply" component={application}/>
-  
-</Switch>
-</BrowserRouter>
+  <Router> 
+      <ScrollToTop>  
+        <Header />
+        <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/about" component={About} />
+        <Route path="/members" component={Member} />
+        <Route path="/quests" component={Quests} />
+        <Route path="/server" component={server} />
+        <Route path="/tutorials" component={Tutorial} />
+        <Route path="/maps" component={Maps} />
+        <Route path="/apply" component={application} />
+        </Switch>
+        <Footer />
+      </ScrollToTop>
+  </Router>
 );
 
 

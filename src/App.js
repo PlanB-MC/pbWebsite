f8b64896b5295f95d3e73608339afafc8e11ab37
MattCSmith <mrmcchow@gmail.com>
2018-09-22 20:57:06 +0100

    Router Test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"; 
+import { BrowserRouter, Switch, Route} from "react-router-dom"; 
 import './Css/App.css';
 
 import LandingPage from './Pages/landingPage';
@@ -9,14 +9,14 @@ import application from './Pages/application'
 
 console.log(process.env.REACT_APP_TEST_TOKEN)
 const App = () => (
-<Router>
+<BrowserRouter>
   <Switch>
   <Route exact path="/" component={LandingPage}/>
   <Route path="/about" component={About}/>
   <Route path="/apply" component={application}/>
   
 </Switch>
-</Router>
+</BrowserRouter>
 );
 
 

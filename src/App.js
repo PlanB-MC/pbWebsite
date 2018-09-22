e44ef611595b06fb73c7b70fa823379175de3486
MattCSmith <mrmcchow@gmail.com>
2018-09-22 20:41:09 +0100

    Removed unused variables

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"; 
 import './Css/App.css';
-import Header from './Components/Global/header';
-import ScrollToTop from './Components/Global/scrollTop';
+
 import LandingPage from './Pages/landingPage';
 import About from './Pages/aboutUs'
-import server from './Pages/serverPage'
-import Member from './Pages/members';
-import Quests from './Pages/questPage';
-import Tutorial from './Pages/tutorialPage';
-import Maps from './Pages/mapPage';
-import Footer from './Components/Global/footer';
+
 import application from './Pages/application'
 
 console.log(process.env.REACT_APP_TEST_TOKEN)
